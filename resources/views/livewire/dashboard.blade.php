cbb911c142428bb5830e2296b4ca9518b1c95d7f
Ryan Martin <bills@giantpilots.com>
2021-01-07 21:32:51 -0500

    styling + general build wip

--- resources/views/livewire/dashboard.blade.php
+++ resources/views/livewire/dashboard.blade.php
@@ -22,7 +22,7 @@
                         <svg class="-ml-1 mr-2 h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
                             <path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd" />
                         </svg>
-                        Journal for {{ Carbon\Carbon::parse($entry_date)->format('F d Y') }}
+                        Complete Journal for {{ Carbon\Carbon::parse($entry_date)->format('F d Y') }}
                     </button>
                 </div>
             @endisset
@@ -76,17 +76,52 @@
             <div class="col-span-10 sm:col-span-5 sm:justify-self-end px-3 sm:pt-2">Gratitude Aside</div>
 
             <div class="col-span-10 text-center p-6 mb-12">
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quibusdam fugit velit dolor rerum molestiae accusamus temporibus exercitationem? Incidunt quae, totam voluptatem officiis ipsa cupiditate eius suscipit aspernatur distinctio itaque. Vel!
+                {{ \App\Models\Quote::all()->random(1)->first()->body }}
             </div>
 
             <div class="col-span-10 px-6 mb-3">
                 <label for="gratitude" class="block text-sm font-medium text-gray-700">
-                    {{ __('Gratitude') }}
+                    {{ __('Today I\'m grateful for...') }}
                 </label>
                 <textarea wire:model="gratitude" name="gratitude" id="gratitude" cols="30" rows="5" class="w-full p-2"></textarea>
+                @error('gratitude')
+                    <div class="form-error">{{ $message }}</div>
+                @else
+                    <div>NO errors</div>
+                @enderror
             </div>
 
             @isset($gratitude)
+                <div class="col-span-10 px-6 mb-3">
+                    <button wire:click="$set('step', 4)" type="button" class="w-full inline-flex items-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                        <!-- Heroicon name: check -->
+                        <svg class="-ml-1 mr-2 h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                            <path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd" />
+                        </svg>
+                        And the next one...
+                    </button>
+                </div>
+            @endisset
+        </div>
+    @endif
+
+    @if($step === 4 )
+        <div class="grid grid-cols-10 bg-gray-200">
+            <div class="col-span-10 sm:col-span-5 text-3xl px-3 pt-2">Daily Habits</div>
+            <div class="col-span-10 sm:col-span-5 sm:justify-self-end px-3 sm:pt-2">Habits Aside</div>
+
+            <div class="col-span-10 text-center p-6 mb-12">
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quibusdam fugit velit dolor rerum molestiae accusamus temporibus exercitationem? Incidunt quae, totam voluptatem officiis ipsa cupiditate eius suscipit aspernatur distinctio itaque. Vel!
+            </div>
+
+            <div class="col-span-10 px-6 mb-3">
+                <label for="habits" class="block text-sm font-medium text-gray-700">
+                    {{ __('habits') }}
+                </label>
+                <textarea wire:model="habits" name="habits" id="habits" cols="30" rows="5" class="w-full p-2"></textarea>
+            </div>
+
+            @isset($habits)
                 <div class="col-span-10 px-6 mb-3">
                     <button wire:click="$set('step', 4)" type="button" class="w-full inline-flex items-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
                         <!-- Heroicon name: check -->
