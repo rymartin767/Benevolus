fb1f2af5e1e0d4c4ba8b5c78dcf8c06e91ba5126
Ryan Martin <bills@giantpilots.com>
2021-01-07 21:32:39 -0500

    added rules() wip

--- app/Http/Livewire/Dashboard.php
+++ app/Http/Livewire/Dashboard.php
@@ -4,22 +4,35 @@ namespace App\Http\Livewire;
 
 use Livewire\Component;
 use App\Models\Source;
-use Carbon\Carbon;
 
 class Dashboard extends Component
 {
     public $sources;
-    public $step = 1;
+    public $step;
 
     public $entry_date;
     public $feel_rating;
     public $feel_reason;
     public $gratitude;
+    public $habits;
 
     
     public function mount()
     {
+        $this->step = 1;
         $this->sources = Source::all();
+    }
+
+    public function rules()
+    {
+        return [
+            'gratitude' => 'required|string|min:5|max:65353'
+        ];
+    }
+
+    public function updated($propertyName)
+    {
+        $this->validateOnly($propertyName);
     }
 
     public function render()
